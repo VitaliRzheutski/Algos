496c7dc6e8f70d32c06a00fdb08bc5ea5b9d2527
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2021-11-22 18:05:06 -0500

    shifting

--- LinkedList.js
+++ LinkedList.js
@@ -48,6 +48,17 @@ class SinglyLinkedList{
         }
         return current;
     }
+    shift(){
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null
+        }
+        return currentHead
+
+    }
 }
 
 var list = new SinglyLinkedList()
