a039e228e700ce07a7889081d35e1781b09887c2
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-01-11 18:18:27 -0500

    added DFS Post order method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -93,6 +93,18 @@ class BinarySearchTree {
         traverse(this.root); //invoke the helper func with the current variable
         return data;
     }
+
+    DFSPostOrder(){
+        //at first then push
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 
