165c2fc770e54038112a074e2530f607805b9d7c
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-01-14 17:33:41 -0500

    added breadth first fraph traversal method

--- graphs.js
+++ graphs.js
@@ -23,6 +23,7 @@ class Graph{
     }
     //GRAPH TRAVERSAL
 
+    //RECURSION
     //func should accept a starting node(vertex)
     depthFirstRecursive(start){
         //create a list to store the end result,to be retuned at the very end
@@ -41,7 +42,7 @@ class Graph{
             result.push(vertex);
             //loop over through the adjacencyList for that vertex
             adjacencyList[vertex].forEach(neighbor => {
-                //if any of those values hanne not been visited 
+                //if any of those values have not been visited 
                 //recursively invoke the helper func with that vertex
                 if(!visited[neighbor]){
                     return dfs(neighbor)
@@ -49,6 +50,66 @@ class Graph{
             });
         })(start);//immediattely ivoked func
 
+        return result;
+    }
+
+    //ITERATIVE
+    //funnct that accept a starting node
+    depthFirstIterative(start){
+        //create a stack to help use keep track of vertices
+        const stack = [start];
+        //create a list to store the end result,to be retuned at the very end
+        const result = [];
+        //create an object to store visited vertices
+        const visited = {};
+        let currentVertex;
+
+        //add the starting vertex to the stack,and mark it visited
+        visited[start] = true;
+        //while the stack has something on it
+        while(stack.length){
+            //pop the next vertex from the stack
+            currentVertex = stack.pop()
+            //need to add currentVertex to result array
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                //if that vertex hasn't been visited yet
+               if(!visited[neighbor]){
+                   //mark it as visited
+                   visited[neighbor] = true;
+                   //push all of its neighbors into the stack
+                   stack.push(neighbor)
+               } 
+            });
+        }
+        return result;
+    }
+    breadthFirst(start){
+        //create a queue and place the starting vertex in it
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        //mark the starting vertex as visited
+        visited[start] = true;
+        //loop as long as there is anything in thr queue
+        while(queue.length){
+            //remove the first vertex from the queue
+            currentVertex = queue.shift();
+            //push it into the array that stores nodes visited
+            result.push(currentVertex);
+            //loop over each vertex in the adjacency list for the vertex you are visiting
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                //if it's not inside the object that stores nodes visited
+                if(!visited[neighbor]){
+                    //mark kit as visited 
+                    visited[neighbor] = true;
+                    //push to the queue
+                    queue.push(neighbor);
+                }
+            });
+        }
         return result;
     }
    
