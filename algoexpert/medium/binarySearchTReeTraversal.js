b906c5a4b1f700ec088004a08d5a282507f621f8
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-02-26 21:58:05 -0500

    binary search tree traversal

---
+++ algoexpert/medium/binarySearchTReeTraversal.js
@@ -0,0 +1,30 @@
+function inOrderTraverse(tree, array) {
+  //left->root->right
+  if (tree) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function preOrderTraverse(tree, array) {
+  //root->left->right
+
+  if (tree) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function postOrderTraverse(tree, array) {
+  //left->right->root
+  if (tree) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+  }
+  return array;
+}
