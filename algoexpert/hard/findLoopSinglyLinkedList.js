7d6ab9d3f28dd45dda44ffbcd535b68c7eda4a9e
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-04-29 12:49:30 -0400

    optimal solution

--- algoexpert/hard/findLoopSinglyLinkedList.js
+++ algoexpert/hard/findLoopSinglyLinkedList.js
@@ -8,3 +8,26 @@ function findLoop(head) {
     head = head.next;
   }
 }
+
+// Time O(N) | Space O(1)
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function findLoop(head) {
+  let p1 = head.next;
+  let p2 = head.next.next;
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next.next;
+  }
+  p1 = head;
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return p1;
+}
