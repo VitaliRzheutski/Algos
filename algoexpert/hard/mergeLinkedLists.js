5331848008e31fd9d3b688cf59332a7af1a12d2a
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-05-03 11:47:58 -0400

    merge linked lists problem

---
+++ algoexpert/hard/mergeLinkedLists.js
@@ -0,0 +1,27 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+// Time: O(n + m) | Space: O(1) where n,m length linked lists
+function mergeLinkedLists(headOne, headTwo) {
+  let p1 = headOne;
+  let p2 = headTwo;
+  let prev = null;
+
+  while (p1 && p2) {
+    if (p1.value < p2.value) {
+      prev = p1;
+      p1 = p1.next;
+    } else {
+      if (prev) prev.next = p2;
+      prev = p2;
+      p2 = p2.next;
+      prev.next = p1;
+    }
+  }
+  if (!p1) prev.next = p2;
+
+  return headOne.value < headTwo.value ? headOne : headTwo;
+}
